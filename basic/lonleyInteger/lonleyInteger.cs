76f4d40dc0d2cde7307b22cb38a206c4a95ef5b4
iman nemati <imun22@gmmail.com>
2021-11-18 19:21:59 +0330

    Add lonleyInteger challenge with answer

---
+++ basic/lonleyInteger/lonleyInteger.cs
@@ -0,0 +1,63 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'lonelyinteger' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int lonelyinteger(List<int> a)
+    {
+        int count = a.Count();
+        
+        for(int i = 0; i < count; i++) {
+            int counter = 0;
+            for(int j = 0; j < count; j++) {
+                if(i == j) continue;
+                
+                if(a[i] == a[j])
+                    counter++;
+            }
+            if(counter == 0) return a[i];
+            counter = 0;
+        }
+        
+        return 0;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        int result = Result.lonelyinteger(a);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
